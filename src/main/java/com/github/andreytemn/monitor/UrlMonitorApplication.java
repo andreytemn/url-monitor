a6d81bb9973987569e7ef53681a344bb963e450c
Andrei Temnikov <temnikov.andrey1@gmail.com>
2023-03-02 21:31:46 +0100

    Create project structure

---
+++ src/main/java/com/github/andreytemn/monitor/UrlMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.github.andreytemn.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/andreytemn/monitor/UrlMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andreytemn.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
